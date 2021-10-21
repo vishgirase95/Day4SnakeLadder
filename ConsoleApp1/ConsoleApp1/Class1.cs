7c8fd8ee9441c55fc12b7738fb41b6722004e753
vish <vishgirase95@gmail.com>
2021-10-22 00:56:15 +0530

    [vishal] refactor rolls die know position corrected

--- ConsoleApp1/ConsoleApp1/Class1.cs
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -6,13 +6,14 @@ namespace ConsoleApp1
 {
     class Class1
     {
-        public int player1 = 0;
 
         public void option()
         {
             Random opt = new Random();
             int ChoseOption = opt.Next(1, 4);
             int dice = opt.Next(1, 7);
+            int player1 = 0;
+
 
             
 
@@ -20,30 +21,36 @@ namespace ConsoleApp1
             {
                 player1 = 0;
                 Console.WriteLine("no play");
+                Console.WriteLine(dice + "dice value");
+
             }
             else if (ChoseOption == 2)
             {
                 player1 = player1 + dice;
                 Console.WriteLine("Ladder");
+                Console.WriteLine(dice + "dice value");
+
 
             }
             else
             {
-                player1 = player1 - dice;
+                player1 = player1 -dice;
                 Console.WriteLine("Snake");
-                
+                Console.WriteLine(dice+"dice value");
+
+
 
             }
-            
 
-         
+
+            player(player1);
         }
 
-        public int player()
+        public void player(int player1)
         {
-            int position = player1;
+            int position = player1+1;
 
-            return position;
+            
         }
 
 
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,12 +13,42 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-            Class1 play = new Class1();
-            play.option();
-            Class1 playerr = new Class1();
-            double position=playerr.player();
-                        
-            Console.WriteLine("player position : "+ position );
+           
+
+
+            Random opt = new Random();
+            int ChoseOption = opt.Next(1, 4);
+            int dice = opt.Next(1, 7);
+            int player1 = 0;
+
+
+
+
+            if (ChoseOption == 1)
+            {
+                player1 = 0;
+                Console.WriteLine("no play");
+                Console.WriteLine(dice + "dice value");
+
+            }
+            else if (ChoseOption == 2)
+            {
+                player1 = player1 + dice;
+                Console.WriteLine("Ladder");
+                Console.WriteLine(dice + "dice value");
+
+
+            }
+            else
+            {
+                player1 = player1 - dice;
+                Console.WriteLine("Snake");
+                Console.WriteLine(dice + "dice value");
+            }
+
+
+            Console.WriteLine("Position :  " + player1);
+
         }
     }
 }
