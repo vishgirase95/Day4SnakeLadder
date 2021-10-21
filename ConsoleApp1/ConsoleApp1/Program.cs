25d4e6e9799a6c0d56921c149d233ae8ebbf61db
vish <vishgirase95@gmail.com>
2021-10-21 22:40:54 +0530

    [vishal] added start position

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int player1 = 0;
+            Console.WriteLine("player position : " + player1);
         }
     }
 }
