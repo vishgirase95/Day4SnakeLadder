57386ebcd325376c029c4637e958df8730f4a429
vish <vishgirase95@gmail.com>
2021-10-23 01:12:35 +0530

    [vishal] added repeat till win 100 & restart at 0

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,39 +16,53 @@ namespace ConsoleApp1
            
 
 
-            Random opt = new Random();
-            int ChoseOption = opt.Next(1, 4);
-            int dice = opt.Next(1, 7);
-            int player1 = 0;
+            
+       
+            int position = 0;
 
-
-
-
-            if (ChoseOption == 1)
+            while (position <= 100 )
+             
             {
-                player1 = 0;
-                Console.WriteLine("no play");
-                Console.WriteLine(dice + "dice value");
 
+                Random opt = new Random();
+                int ChoseOption = opt.Next(1, 4);
+                int dice = opt.Next(1, 7);
+
+                if (ChoseOption == 1)
+                {
+                    
+                    Console.WriteLine($"No Play dice is {dice} and position value is :{position}");
+
+                    continue;
+
+                }
+                else if (ChoseOption == 2)
+                {
+                    position = position + dice;
+
+                    Console.WriteLine($"Ladder dice is {dice} and position value is :{position}");
+
+
+                }
+                else
+                {
+                    position = position - dice;
+
+                    Console.WriteLine($"Snake dice is {dice} and position value is :{position}");
+                }
+                if (position < 0)
+                {
+                    position = 0;
+                }
             }
-            else if (ChoseOption == 2)
-            {
-                player1 = player1 + dice;
-                Console.WriteLine("Ladder");
-                Console.WriteLine(dice + "dice value");
-
-
-            }
+            if (position>=100)
+            Console.WriteLine("Its win , Your Position is :  " + position);
             else
             {
-                player1 = player1 - dice;
-                Console.WriteLine("Snake");
-                Console.WriteLine(dice + "dice value");
+                Console.WriteLine("you loss , Your Position is :  " + position);
+
             }
-
-
-            Console.WriteLine("Position :  " + player1);
-
         }
+
     }
 }
